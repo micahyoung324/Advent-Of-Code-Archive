4fff1a364d8373776a28b05dfc665618790bef1e
Micah Young <micahyoung324@proton.me>
2025-12-18 15:53:17 -0700

    Added 2025 Day 5 Solution (Only for first part)
    Will add second part solution someday eventually.

---
+++ 2025/05/main.py
@@ -0,0 +1,50 @@
+FILENAME = "input.txt"
+
+
+def get_input() -> tuple[list[tuple[int,int]], list[int]]:
+    fresh_ranges = list()
+    ingredient_IDs = list()
+
+    range_flag = True
+
+    with open(FILENAME) as file:
+        for line in file.readlines():
+            line = line.strip()
+
+            if line == "":
+                range_flag = False
+                continue
+
+            if range_flag:
+                fresh_ranges.append(tuple(map(int, line.split('-'))))
+                
+            else:
+                ingredient_IDs.append(int(line))
+
+    return fresh_ranges, ingredient_IDs
+
+
+def part_1():
+    fresh_ranges, ingredient_IDs = get_input()
+    result = 0
+
+    for curr_ID in ingredient_IDs:
+        for min_r, max_r in fresh_ranges:
+            if min_r <= curr_ID <= max_r:
+                result += 1
+                break
+
+    print(f"Answer is {result}")
+
+
+def part_2():
+    fresh_ranges, ingredient_IDs = get_input()
+    result = 0
+
+    print(f"Answer is {result}")
+
+
+if __name__ == "__main__":
+    # part_1()
+    # part_2()
+    pass
